add30d2b95ff2e461964792fb97d4fd359722617
Farid <farid.fv1@gmail.com>
2025-07-22 16:32:46 +0400

    item entity created

---
+++ invoice/src/main/java/az/cybernet/invoice/entity/ItemEntity.java
@@ -0,0 +1,25 @@
+package az.cybernet.invoice.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = PRIVATE)
+public class ItemEntity {
+    Long id;
+    String name;
+    BigDecimal price;
+    Integer quantity;
+    Boolean isActive;
+    BigDecimal totalPrice;
+    MeasurementEntity measurement;
+    InvoiceEntity invoice;
+    List<OperationEntity> operations;
+}
