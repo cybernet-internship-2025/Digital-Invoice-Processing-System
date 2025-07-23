e5d93c8a6f572175cf212d5a672c801ec71b7355
Farid <mustafayevfarid2002@gmail.com>
2025-07-23 23:18:43 +0400

    change senderUserId and recipientUserId to senderTaxId and recipientTaxId

--- invoice/src/main/java/az/cybernet/invoice/service/abstraction/InvoiceService.java
+++ invoice/src/main/java/az/cybernet/invoice/service/abstraction/InvoiceService.java
@@ -1,4 +1,9 @@
 package az.cybernet.invoice.service.abstraction;
 
+import az.cybernet.invoice.dto.request.create.CreateInvoiceRequest;
+import az.cybernet.invoice.dto.response.InvoiceResponse;
+
 public interface InvoiceService {
+    InvoiceResponse saveInvoice(CreateInvoiceRequest invoiceRequest);
+
 }
