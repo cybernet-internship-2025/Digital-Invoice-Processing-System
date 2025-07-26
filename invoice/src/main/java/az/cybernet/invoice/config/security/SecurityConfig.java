889e75ea2667ee4776fe3b1aadf9090e50c096d6
Farid <mustafayevfarid2002@gmail.com>
2025-07-27 01:31:02 +0400

    change csrf

--- invoice/src/main/java/az/cybernet/invoice/config/security/SecurityConfig.java
+++ invoice/src/main/java/az/cybernet/invoice/config/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package az.cybernet.invoice.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,7 +24,7 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .httpBasic(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
