88d1671243177b8887aa4aec3642b246e23768f4
Farid <mustafayevfarid2002@gmail.com>
2025-07-27 01:34:33 +0400

    again changes csrf logic

--- invoice/src/main/java/az/cybernet/invoice/config/security/SecurityConfig.java
+++ invoice/src/main/java/az/cybernet/invoice/config/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .httpBasic(withDefaults())
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"));
 
         return http.build();
     }
