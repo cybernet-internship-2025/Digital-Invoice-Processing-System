94dfb179757ca509306f10f7f7ca61a215d76350
Mehdi Asadov <mehdiesedof@gmail.com>
2025-08-01 12:37:54 +0400

    Add item service

--- invoice/src/main/java/az/cybernet/invoice/dto/request/item/UpdateItemRequest.java
+++ invoice/src/main/java/az/cybernet/invoice/dto/request/item/UpdateItemRequest.java
@@ -1,16 +1,16 @@
 package az.cybernet.invoice.dto.request.item;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class UpdateItemRequest {
+    @NotNull(message = "Item ID cannot be null")
     Long id;
-    String productName;
-    BigDecimal totalPrice;
+    String name;
     BigDecimal unitPrice;
     Integer quantity;
     String measurementName;
-    Long invoiceId;
 }
