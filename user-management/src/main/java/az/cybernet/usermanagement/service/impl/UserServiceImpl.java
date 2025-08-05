f9619a25e9715999ca75f2400f0b9b300a004a48
Mansura <badalovamansura40@gmail.com>
2025-08-05 15:26:29 +0400

    removing UserControllerTest

--- user-management/src/main/java/az/cybernet/usermanagement/service/impl/UserServiceImpl.java
+++ user-management/src/main/java/az/cybernet/usermanagement/service/impl/UserServiceImpl.java
@@ -89,7 +89,7 @@ public class UserServiceImpl implements UserService {
     }
 
 
-    public UserEntity fetchUserIfExist(String taxId) {
+    private UserEntity fetchUserIfExist(String taxId) {
         return  userRepository.findUserByTaxId(taxId)
                 .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND.getCode(), USER_NOT_FOUND.getMessage()));
     }
