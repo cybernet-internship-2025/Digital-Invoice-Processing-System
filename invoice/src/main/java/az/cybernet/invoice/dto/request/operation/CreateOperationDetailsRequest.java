28e150f5dc9d489b17c232fb316299b5089fd103
Rauf Abbasov <abbasovraufshamilovich@gmail.com>
2025-08-07 12:15:49 +0400

    ItemStatus import was added to classes

--- invoice/src/main/java/az/cybernet/invoice/dto/request/operation/CreateOperationDetailsRequest.java
+++ invoice/src/main/java/az/cybernet/invoice/dto/request/operation/CreateOperationDetailsRequest.java
@@ -1,5 +1,6 @@
 package az.cybernet.invoice.dto.request.operation;
 
+import az.cybernet.invoice.enums.ItemStatus;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import static lombok.AccessLevel.PRIVATE;
--- invoice/src/main/java/az/cybernet/invoice/entity/OperationDetailsEntity.java
+++ invoice/src/main/java/az/cybernet/invoice/entity/OperationDetailsEntity.java
@@ -1,5 +1,6 @@
 package az.cybernet.invoice.entity;
 
+import az.cybernet.invoice.enums.ItemStatus;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import static lombok.AccessLevel.PRIVATE;
