02444d2133da054030286e3d09768de5a5ff2789
Cahidbayramov0 <bayramov.jahid@gmail.com>
2025-08-07 17:40:05 +0400

    add encapsulation

--- invoice/src/main/java/az/cybernet/invoice/controller/OperationDetailsController.java
+++ invoice/src/main/java/az/cybernet/invoice/controller/OperationDetailsController.java
@@ -30,5 +30,5 @@ public class OperationDetailsController {
         return service.findAll();
     }
 
-   
+
 }
--- invoice/src/main/java/az/cybernet/invoice/dto/response/operation/OperationDetailsResponse.java
+++ invoice/src/main/java/az/cybernet/invoice/dto/response/operation/OperationDetailsResponse.java
@@ -2,12 +2,16 @@ package az.cybernet.invoice.dto.response.operation;
 
 import az.cybernet.invoice.enums.ItemStatus;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import static lombok.AccessLevel.PRIVATE;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
 public class OperationDetailsResponse {
     Long id;
     Long operationId;
