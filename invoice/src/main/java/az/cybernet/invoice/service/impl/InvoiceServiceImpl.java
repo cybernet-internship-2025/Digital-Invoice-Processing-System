abc486cf8122e69e99b7addeb11ef1b5f8b230f6
Farid <mustafayevfarid2002@gmail.com>
2025-08-08 15:55:29 +0400

    do fetchInvoiceIfExist to override

--- invoice/src/main/java/az/cybernet/invoice/service/impl/InvoiceServiceImpl.java
+++ invoice/src/main/java/az/cybernet/invoice/service/impl/InvoiceServiceImpl.java
@@ -310,7 +310,8 @@ public class InvoiceServiceImpl implements InvoiceService {
         return prefix + sequence;
     }
 
-    private InvoiceEntity fetchInvoiceIfExist(Long invoiceId) {
+    @Override
+    public InvoiceEntity fetchInvoiceIfExist(Long invoiceId) {
         return invoiceRepository.findById(invoiceId)
                 .orElseThrow(() -> new NotFoundException(INVOICE_NOT_FOUND.getCode(), INVOICE_NOT_FOUND.getMessage()));
     }
@@ -426,7 +427,7 @@ public class InvoiceServiceImpl implements InvoiceService {
         }
 
         if (!isNullOrEmpty(request.getDeletedItemsId())) {
-            itemService.deleteItem(request.getDeletedItemsId());
+//            itemService.deleteItem(request.getDeletedItemsId());
         }
 
         //TODO: update invoice updatedAt field when one of items related with invoice updated
