13c5d171820cab18a286e76daddeaa873fab4769
Mehdi Asadov <mehdiesedof@gmail.com>
2025-09-05 15:30:46 +0400

    Delete files related to PDF in the file-storage service

--- file-storage/src/main/java/az/cybernet/filestorage/FileStorageApplication.java
+++ file-storage/src/main/java/az/cybernet/filestorage/FileStorageApplication.java
@@ -2,10 +2,8 @@ package az.cybernet.filestorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "az.cybernet.filestorage.client")
 public class FileStorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileStorageApplication.class, args);
